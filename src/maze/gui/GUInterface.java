f8a16fbec2b4571d5b370ccafd6f26da18674bd2
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-04-16 17:56:52 +0100

    Class MazePanel created and added to GUInterface

--- src/maze/gui/GUInterface.java
+++ src/maze/gui/GUInterface.java
@@ -1,46 +1,25 @@
 package maze.gui;
 
-import maze.gui.*;
-
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class GUInterface.
- */
 public class GUInterface {
 
-	/** The frame. */
 	private JFrame frame;
-	
-	/** The main menu. */
-	private MainMenuPanel mainMenu;
-	
-	/** The game panel. */
-	private MazePanel gamePanel;
-	
-	/** The menu bar. */
-	private JMenuBar menuBar;
-	
 
 	/**
 	 * Launch the application.
-	 *
-	 * @param args the arguments
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
-			@Override
 			public void run() {
 				try {
 					GUInterface window = new GUInterface();
@@ -63,49 +42,20 @@ public class GUInterface {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
+		frame.setPreferredSize(new Dimension(500, 500));
 		
-		JMenu mnFile = new JMenu("File");
-		menuBar.add(mnFile);
+		// MAZE PANEL
+		MazePanel mazePanel = new MazePanel();
+		frame.getContentPane().add(mazePanel, BorderLayout.CENTER);
 		
-		JMenuItem mntmNewGame = new JMenuItem("New Game");
-		mnFile.add(mntmNewGame);
-		
-		JMenuItem mntmSaveGame = new JMenuItem("Save Game");
-		mnFile.add(mntmSaveGame);
-		
-		JMenuItem mntmLoadGame = new JMenuItem("Load Game");
-		mnFile.add(mntmLoadGame);
-		
-		JSeparator separator_1 = new JSeparator();
-		mnFile.add(separator_1);
-		
-		JMenuItem mntmSettings = new JMenuItem("Settings");
-		mnFile.add(mntmSettings);
-		
-		JSeparator separator_2 = new JSeparator();
-		mnFile.add(separator_2);
-		
-		JMenuItem mntmExitGame = new JMenuItem("Exit Game");
-		mnFile.add(mntmExitGame);
-		
-		JMenu mnAbout = new JMenu("About");
-		menuBar.add(mnAbout);
-		
-		JMenuItem mntmHowToPlay = new JMenuItem("How to Play");
-		mnAbout.add(mntmHowToPlay);
-		
-		JSeparator separator = new JSeparator();
-		mnAbout.add(separator);
-		
-		JMenuItem mntmAbout = new JMenuItem("About");
-		mnAbout.add(mntmAbout);
+		// BUTTONS PANEL
+		JPanel buttons = new JPanel();
+		frame.getContentPane().add(buttons, BorderLayout.SOUTH);
 		
 		
 	}
--- src/maze/gui/MazePanel.java
+++ src/maze/gui/MazePanel.java
@@ -1,18 +1,43 @@
 package maze.gui;
 
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class MazePanel.
- */
-public class MazePanel extends JPanel {
-
-	/**
-	 * Create the panel.
-	 */
-	public MazePanel() {
-
+public class MazePanel extends JPanel implements MouseListener, MouseMotionListener
+{
+	private int x1=0, y1=0, x2=0, y2=0;
+	
+	public MazePanel()
+	{
+		addMouseListener(this);
+		addMouseMotionListener(this); 
 	}
-
+	
+	public void paintComponent(Graphics g) 
+	{
+		super.paintComponent(g); 
+		g.setColor(Color.BLUE);
+		g.fillOval(x1, y1, x2 - x1 + 1, y2 - y1 + 1);
+	}
+	
+	public void mousePressed(MouseEvent e) 
+	{
+		x2 = x1 = e.getX();
+		y2 = y1 = e.getY();
+		repaint();
+	}
+	
+	public void mouseDragged(MouseEvent e) 
+	{ 
+		x2 = e.getX();
+		y2 = e.getY();
+		repaint();
+	}
+	
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseMoved(MouseEvent arg0) {}
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
 }
