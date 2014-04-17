726dcf9caddce7290fdb9898cf10638b8116ebcf
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-04-17 02:42:42 +0100

    Panel re-arrangement

--- src/maze/gui/GUInterface.java
+++ src/maze/gui/GUInterface.java
@@ -6,9 +6,15 @@ import maze.logic.*;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
-
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GUInterface {
+	
 	
 	/**
 	 * Launch the application.
@@ -18,12 +24,30 @@ public class GUInterface {
 		try {
 			JFrame frame = new JFrame("MAZE GAME");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setPreferredSize(new Dimension(500, 500));
+			frame.setPreferredSize(new Dimension(500, 550));
+			frame.getContentPane().setLayout(new BorderLayout(0, 0));
 			
-			// MAZE PANEL
+			// Main Panel
+			JPanel mainPanel = new JPanel();
+			frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+			mainPanel.setLayout(new BorderLayout(0, 0));
+			
+			// Maze Panel
 			MazePanel mazePanel = new MazePanel();
+			mainPanel.add(mazePanel, BorderLayout.CENTER);
 			mazePanel.setFocusable(true);
-			frame.getContentPane().add(mazePanel, BorderLayout.CENTER);
+
+			// Buttons Panel
+			JPanel buttonsPanel = new JPanel();
+			mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
+			// Button - NEW GAME
+			JButton newGame = new JButton("New Game");
+			buttonsPanel.add(newGame);
+
+			// Button - EXIT GAME
+			JButton exitGame = new JButton("Exit Game");
+			buttonsPanel.add(exitGame);
 			
 			frame.pack();
 			frame.setVisible(true);
