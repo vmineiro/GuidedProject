dccb3677cf6d32edf215c373dc50b7d96a1b861a
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-04-17 01:07:16 +0100

    Minor code cleanup

--- src/maze/gui/GUInterface.java
+++ src/maze/gui/GUInterface.java
@@ -2,61 +2,40 @@ package maze.gui;
 
 import maze.logic.*;
 
-import java.awt.Color;
+
 import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Graphics;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 import java.awt.BorderLayout;
 
 
 public class GUInterface {
-
-	private JFrame frame;
 	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) 
-	{		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					
-					GUInterface window = new GUInterface();
-					window.frame.setVisible(true);
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+	{
+		try {
+			JFrame frame = new JFrame("MAZE GAME");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setPreferredSize(new Dimension(500, 500));
+			
+			// MAZE PANEL
+			MazePanel mazePanel = new MazePanel();
+			mazePanel.setFocusable(true);
+			frame.getContentPane().add(mazePanel, BorderLayout.CENTER);
+			
+			frame.pack();
+			frame.setVisible(true);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public GUInterface() 
-	{
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		
-		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
-		
-		// MAZE PANEL
-		MazePanel mazePanel = new MazePanel();
-		mazePanel.setFocusable(true);
-		frame.getContentPane().add(mazePanel, BorderLayout.CENTER);
-	}
+	public GUInterface() {}
 
 }
