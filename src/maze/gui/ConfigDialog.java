890d4e8e7b5f1b4b84e5cfcbdd1f6170670114e5
Rodolfo Rodrigues <rodolfo.raar@gmail.com>
2014-04-17 21:02:07 +0100

    Class ConfigDialog - Keylisteners added to key assignment buttons

--- src/maze/gui/ConfigDialog.java
+++ src/maze/gui/ConfigDialog.java
@@ -3,18 +3,26 @@ package maze.gui;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 import java.awt.FlowLayout;
+
 import javax.swing.JSpinner;
 import javax.swing.BoxLayout;
+
 import java.awt.Component;
+
 import javax.swing.JSlider;
 import javax.swing.JRadioButton;
 
@@ -47,6 +55,12 @@ public class ConfigDialog extends JDialog
     private int selMazeSize;
     private int selNDragons;
     private int selMode;
+    
+    private int selUpKey = KeyEvent.VK_UP;
+    private int selDownKey = KeyEvent.VK_DOWN;
+    private int selLeftKey = KeyEvent.VK_LEFT;
+    private int selRightKey = KeyEvent.VK_RIGHT;
+    private int selEagleKey = KeyEvent.VK_E;
 	
 	public ConfigDialog(JFrame frame, boolean modal, String myMessage)
 	{
@@ -107,16 +121,25 @@ public class ConfigDialog extends JDialog
 		keysLabelPanel = new JPanel();
 		keysLabelPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		keyLabel = new JLabel("Reassign Keyboard Keys:");
+		keyLabel = new JLabel("Reassign Keyboard Keys - Click Button and Press Keyboard Key to Assign");
 			
 		keysPanel = new JPanel();
 		keysPanel.setLayout(new BorderLayout(0, 0));
 		
 		btnUp = new JButton("Up");
+		btnUp.addKeyListener(new UpConfigListener());
+		
 		btnDown = new JButton("Down");
+		btnDown.addKeyListener(new DownConfigListener());
+		
 		btnLeft = new JButton("Left");
+		btnLeft.addKeyListener(new LeftConfigListener());
+		
 		btnRight = new JButton("Right");
+		btnRight.addKeyListener(new RightConfigListener());
+		
 		btnEagle = new JButton("Eagle");
+		btnEagle.addKeyListener(new EagleConfigListener());
 		
 		// Buttons Panel
 		buttonsPanel = new JPanel();
@@ -185,6 +208,31 @@ public class ConfigDialog extends JDialog
 		return selMode;
 	}
 	
+	public int getSelUpKey()
+	{
+		return selUpKey;
+	}
+	
+	public int getSelDownKey()
+	{
+		return selDownKey;
+	}
+	
+	public int getSelLeftKey()
+	{
+		return selLeftKey;
+	}
+	
+	public int getSelRightKey()
+	{
+		return selRightKey;
+	}
+	
+	public int getSelEagleKey()
+	{
+		return selEagleKey;
+	}
+	
 	private class saveConfigListener implements ActionListener
 	{
 		@Override
@@ -220,5 +268,88 @@ public class ConfigDialog extends JDialog
 		}	
 	}
 	
+	private class UpConfigListener implements KeyListener
+	{
+
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			selUpKey = e.getKeyCode();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+		
+	}
 	
+	private class DownConfigListener implements KeyListener
+	{
+
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			selDownKey = e.getKeyCode();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+		
+	}
+	
+	private class LeftConfigListener implements KeyListener
+	{
+
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			selLeftKey = e.getKeyCode();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+		
+	}
+	
+	private class RightConfigListener implements KeyListener
+	{
+
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			selRightKey = e.getKeyCode();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+		
+	}
+	
+	private class EagleConfigListener implements KeyListener
+	{
+
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			selEagleKey = e.getKeyCode();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {}
+
+		@Override
+		public void keyReleased(KeyEvent e) {}
+		
+	}
 }
